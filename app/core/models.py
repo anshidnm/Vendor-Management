9fd27bbfe070e2b8cc521318183aaf878e2ead1d
anshid <anshidnm@gmail.com>
2024-04-27 15:43:53 +0530

    pre commit auto format

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    """
+    The Base model class for all other models
+    to inherit including created and updated timestamp
+    with active or not
+    """
+
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+    class Meta:
+        abstract = True
